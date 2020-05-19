c1fea5f87ed619932c9c5dc83fabe40b049538da
L0615T1C5-216AC-9437 <60281819+L0615T1C5-216AC-9437@users.noreply.github.com>
2020-05-19 10:46:49 -0400

    Timer

--- src/main/java/A/async.java
+++ src/main/java/A/async.java
@@ -28,48 +28,79 @@ public class async extends Thread {
         } catch (Exception e) {
         }
         Log.info("async running");
+        int timer = 10;
+        int minutes = timer-1;
+        int seconds = 59;
+        String second = "";
+        String minute = "";
+
+        if (byteCode.has("async")) {
+            data = byteCode.get("async");
+            if (data.has("list")) {
+                list = data.getJSONObject("list");
+            }
+            if (data.has("timer")) {
+                timer = data.getInt("timer");
+                minutes = timer-1;
+                seconds = 59;
+            }
+        }
         while (MainT.isAlive()) {
-            //sleep
-            if (byteCode.has("async")) {
-                data = byteCode.get("async");
-                if (data.has("timer")) {
-                    try {
-                        TimeUnit.MINUTES.sleep(data.getInt("timer"));
-                    } catch (Exception e) {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                seconds--;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (seconds == -1) {
+                if (minutes == 0) {
+                    if (byteCode.has("async")) {
+                        data = byteCode.get("async");
+                        if (data.has("list")) {
+                            list = data.getJSONObject("list");
+                        }
+                    }
+                    for (Player p : Vars.playerGroup.all()) {
+                        if (list.has(p.uuid)) continue;
+                        Call.onWorldDataBegin(p.con);
+                        netServer.sendWorldData(p);
+                        Call.onInfoToast(p.con, "Auto-Sync completed.", 5);
+                        try {
+                            TimeUnit.SECONDS.sleep(1);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    if (byteCode.has("async")) {
+                        data = byteCode.get("async");
+                        if (data.has("list")) {
+                            list = data.getJSONObject("list");
+                        }
+                        if (data.has("timer")) {
+                            timer = data.getInt("timer");
+                            minutes = timer-1;
+                            seconds = 59;
+                        }
                     }
                 } else {
-                    Log.err("Invalid file - " + System.getProperty("user.home") + "/mind_db/async.cn - does not contain JSON key `timer`");
-                    Log.info("Reset file using command `async-clear`");
-                    try {
-                        TimeUnit.MINUTES.sleep(10);
-                    } catch (Exception e) {
-                    }
+                    minutes--;
+                    seconds = 59;
                 }
+            }
+            if (seconds < 10) {
+                second = "0";
             } else {
-                Log.err("Missing file - " + System.getProperty("user.home") + "/mind_db/async.cn");
-                Log.info("Reset file using command `async-clear`");
-                try {
-                    TimeUnit.MINUTES.sleep(10);
-                } catch (Exception e) {
-                }
+                second = "";
             }
-            ///sleep
-            if (byteCode.has("async")) {
-                data = byteCode.get("async");
-                if (data.has("list")) {
-                    list = data.getJSONObject("list");
-                }
+            if (minutes < 10) {
+                minute = "0";
+            } else {
+                minute = "";
             }
-            for (Player p : Vars.playerGroup.all()) {
+            for (Player p : playerGroup.all()) {
                 if (list.has(p.uuid)) continue;
-                Call.onWorldDataBegin(p.con);
-                netServer.sendWorldData(p);
-                Call.onInfoToast(p.con, "Auto-Sync completed.", 5);
-                try {
-                    TimeUnit.SECONDS.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                Call.onInfoPopup(p.con, "Async in "+minute+minutes+":"+second+seconds, 1, 16, 10, 10, 10, 10);
             }
         }
     }
